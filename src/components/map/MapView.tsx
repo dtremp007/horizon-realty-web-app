dd6e72537ca3f0316275a013a9126bf3a25986c2
David Rempel <ddrempti@gmail.com>
2022-09-30 19:26:47 -0600

    Some stuff with the map

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,13 +1,11 @@
-import dynamic from "next/dynamic";
-import type { MapRef } from "react-map-gl";
+import Map, { MapRef, ViewStateChangeEvent } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Marker } from "react-map-gl";
+import { GrLocation } from "react-icons/gr";
 import { useRef, useState, useContext, useEffect } from "react";
 import useSupercluster from "use-supercluster";
 import type { BBox } from "geojson";
 import ListingsContext from "../../context/listingsContext/listingsContext";
-
-const Map = dynamic(import("react-map-gl"))
 
 //TODO: Add constraints to how far out use can zoom using turf. Check out this page https://visgl.github.io/react-map-gl/docs/get-started/state-management
 //TODO: Produce a popup, when a listing is pressed. Also, make better icons.
