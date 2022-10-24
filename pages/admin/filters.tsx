0f307b61d0870515362f0b38b4ef55a8a6ef9903
David Rempel <ddrempti@gmail.com>
2022-10-24 16:45:25 -0600

    Filters get fetched from JSON instead of Firestore.

--- pages/admin/filters.tsx
+++ pages/admin/filters.tsx
@@ -160,12 +160,6 @@ const AdminFilters: NextPage<AdminFilterProps> = ({
       });
   };
 
-  useEffect(() => {
-    if (state.savedFilters.size === 0) {
-        resetFilters();
-    }
-  }, []);
-
   const saveFilters = async () => {
     const filters = transformFilters(state.savedFilters, [
       updateFallbackValues,
@@ -333,14 +327,14 @@ function transformFilters(
 export const getServerSideProps: GetServerSideProps = async () => {
   const metadataFile = path.join(process.cwd(), "lib", "metadata.json");
   const metadata = JSON.parse(readFileSync(metadataFile, { encoding: "utf8" }));
-    // const filtersFile = path.join(process.cwd(), "lib", "filters.json");
-    // const filters = JSON.parse(
-    //   readFileSync(filtersFile, { encoding: "utf8" }) // I removed .toString(), I'm not sure why it was here. I don't think it's necessary.
-    // ).filters;
-    const querySnapshot = await getDocs(collection(db, "filters"));
-    const filters = querySnapshot.docs.map((filter) => {
-      return filter.data();
-    });
+    const filtersFile = path.join(process.cwd(), "lib", "filters.json");
+    const filters = JSON.parse(
+      readFileSync(filtersFile, { encoding: "utf8" }) // I removed .toString(), I'm not sure why it was here. I don't think it's necessary.
+    ).filters;
+    // const querySnapshot = await getDocs(collection(db, "filters"));
+    // const filters = querySnapshot.docs.map((filter) => {
+    //   return filter.data();
+    // });
 
   return {
     props: {
