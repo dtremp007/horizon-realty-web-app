96e4905465e5981e56e2f160d06c0fc3d4d7c731
David Rempel <ddrempti@gmail.com>
2022-10-31 10:44:34 -0700

    Type

--- src/components/filter_v2/ListingBuffet.tsx
+++ src/components/filter_v2/ListingBuffet.tsx
@@ -72,7 +72,7 @@ function getIcon(type: string) {
   switch (type) {
     case "CASA":
       return <Image src={detailedHouseIcon} />;
-    case "LOTE_RESIDENCIALES":
+    case "LOTES_RESIDENCIALES":
     case "LOTE":
       return lotIcon;
     case "BODEGA":
