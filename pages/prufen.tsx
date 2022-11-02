4e982166afa0902671a3732fb72876ae22da22a2
David Rempel <ddrempti@gmail.com>
2022-11-02 10:48:43 -0700

    Bug

--- pages/prufen.tsx
+++ pages/prufen.tsx
@@ -1,10 +1,10 @@
 import { Group } from "@mantine/core";
-import RenderForm from "../src/components/admin/form-builder-2/RenderForm";
+// import RenderForm from "../src/components/admin/form-builder-2/RenderForm";
 
 const PrufenPage = () => {
   return (
     <Group mt={78}>
-      <RenderForm />
+      {/* <RenderForm /> */}
     </Group>
   );
 };
