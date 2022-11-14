57ad7991e11b5f1355996c45aad611347716ad9a
David Rempel <ddrempti@gmail.com>
2022-11-13 19:35:07 -0600

    Propiedades now goes directly to /categories page. Added sold category.

--- src/layouts/AltMainLayout.tsx
+++ src/layouts/AltMainLayout.tsx
@@ -47,49 +47,50 @@ const links: Links[] = [
     isActive: (router) =>
       router.pathname === "/listings" && router.query.view === "map",
   },
-  {
-    label: "Propiedades",
-    containsChildren: true,
-    childLinks: [
-      {
-        href: {
-          pathname: "/listings",
-          query: { listingType: "CASA" },
-        },
-        label: "Casas",
-        isActive: (router) =>
-          "/listings" && router.query.listingType === "CASA",
-      },
-      {
-        href: {
-          pathname: "/listings",
-          query: { listingType: "BODEGA" },
-        },
-        label: "Casa Bodegas",
-        isActive: (router) =>
-          "/listings" && router.query.listingType === "BODEGA",
-      },
-      {
-        href: {
-          pathname: "/listings",
-          query: { listingType: "APARTMENTS" },
-        },
-        label: "Apartments",
-        isActive: (router) =>
-          "/listings" && router.query.listingType === "APARTMENTS",
-      },
-      {
-        href: {
-          pathname: "/listings",
-          query: { filter: "LOTES_RESIDENCIALES" },
-        },
-        label: "Lotes",
-        isActive: (router) =>
-          "/listings" && router.query.filter === "LOTES_RESIDENCIALES",
-      },
-    ],
-    isActive: (router) => false,
-  },
+  { href: "/categories", label: "Propiedades", isActive: (router) => router.pathname === "/categories" },
+  //   {
+  //     label: "Propiedades",
+  //     containsChildren: true,
+  //     childLinks: [
+  //       {
+  //         href: {
+  //           pathname: "/listings",
+  //           query: { listingType: "CASA" },
+  //         },
+  //         label: "Casas",
+  //         isActive: (router) =>
+  //           "/listings" && router.query.listingType === "CASA",
+  //       },
+  //       {
+  //         href: {
+  //           pathname: "/listings",
+  //           query: { listingType: "BODEGA" },
+  //         },
+  //         label: "Casa Bodegas",
+  //         isActive: (router) =>
+  //           "/listings" && router.query.listingType === "BODEGA",
+  //       },
+  //       {
+  //         href: {
+  //           pathname: "/listings",
+  //           query: { listingType: "APARTMENTS" },
+  //         },
+  //         label: "Apartments",
+  //         isActive: (router) =>
+  //           "/listings" && router.query.listingType === "APARTMENTS",
+  //       },
+  //       {
+  //         href: {
+  //           pathname: "/listings",
+  //           query: { filter: "LOTES_RESIDENCIALES" },
+  //         },
+  //         label: "Lotes",
+  //         isActive: (router) =>
+  //           "/listings" && router.query.filter === "LOTES_RESIDENCIALES",
+  //       },
+  //     ],
+  //     isActive: (router) => false,
+  //   },
 ];
 
 const AltMainLayout = ({ children }: Props) => {
